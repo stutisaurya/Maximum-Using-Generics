92eda472aa26574da13a6de962d63f6a60394244
stutisaurya <stuti.saurya@gmail.com>
2021-06-28 19:23:21 +0530

    Added:Maximumstrings

--- src/com/Generics/FindMax.java
+++ src/com/Generics/FindMax.java
@@ -23,12 +23,25 @@ public class FindMax {
 		return max;
 	}
 
+	public static String checkMaximum(String s1, String s2, String s3) {
+		String max = s1;
+		if (s2.compareTo(max) > 0) {
+			max = s2;
+		}
+		if (s3.compareTo(max) > 0) {
+			max = s3;
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
 
 		int c = checkMaximum(10, 23, 15);
 		Double d = checkMaximum(12.5, 13.4, 22.0);
+		String s = checkMaximum("Apple", "Grapes", "Banana");
 		System.out.println("The Maximum int value is : " + c);
 		System.out.println("The Maximum float value is : " + d);
+		System.out.println("The Maximum String value is : " + s);
 	}
 
 }
