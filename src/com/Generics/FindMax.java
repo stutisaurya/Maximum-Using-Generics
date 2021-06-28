f788d69642add0e684fa24fb9608a38b424f4872
stutisaurya <stuti.saurya@gmail.com>
2021-06-28 19:13:45 +0530

    Added:MaximumFloats

--- src/com/Generics/FindMax.java
+++ src/com/Generics/FindMax.java
@@ -12,10 +12,23 @@ public class FindMax {
 		return max;
 	}
 
+	public static Double checkMaximum(Double d, Double e, Double f) {
+		Double max = d;
+		if (e.compareTo(max) > 0) {
+			max = e;
+		}
+		if (f.compareTo(max) > 0) {
+			max = f;
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
 
 		int c = checkMaximum(10, 23, 15);
-		System.out.println("The Maximum value is : " + c);
+		Double d = checkMaximum(12.5, 13.4, 22.0);
+		System.out.println("The Maximum int value is : " + c);
+		System.out.println("The Maximum float value is : " + d);
 	}
 
 }
