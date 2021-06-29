12ac1094e71e4c27c18c11600eb55171f741d5de
stutisaurya <stuti.saurya@gmail.com>
2021-06-29 12:53:01 +0530

    Added:Refactor1

--- src/com/Generics/FindMax.java
+++ src/com/Generics/FindMax.java
@@ -1,47 +1,19 @@
 package com.Generics;
 
 public class FindMax {
-	public static int checkMaximum(Integer x, Integer y, Integer z) {
-		Integer max = x;
-		if (y.compareTo(max) > 0) {
+	public <T extends Comparable<T>> T maxOfAnything(T x, T y, T z) {
+		T max = x;
+		if (y.compareTo(max) > 0)
 			max = y;
-		}
-		if (z.compareTo(max) > 0) {
+		if (z.compareTo(max) > 0)
 			max = z;
-		}
-		return max;
-	}
-
-	public static Double checkMaximum(Double d, Double e, Double f) {
-		Double max = d;
-		if (e.compareTo(max) > 0) {
-			max = e;
-		}
-		if (f.compareTo(max) > 0) {
-			max = f;
-		}
-		return max;
-	}
-
-	public static String checkMaximum(String s1, String s2, String s3) {
-		String max = s1;
-		if (s2.compareTo(max) > 0) {
-			max = s2;
-		}
-		if (s3.compareTo(max) > 0) {
-			max = s3;
-		}
 		return max;
 	}
 
 	public static void main(String[] args) {
-
-		int c = checkMaximum(10, 23, 15);
-		Double d = checkMaximum(12.5, 13.4, 22.0);
-		String s = checkMaximum("Apple", "Grapes", "Banana");
-		System.out.println("The Maximum int value is : " + c);
-		System.out.println("The Maximum float value is : " + d);
-		System.out.println("The Maximum String value is : " + s);
+		FindMax m = new FindMax();
+		System.out.println(m.maxOfAnything(10, 23, 15));
+		System.out.println(m.maxOfAnything(12.5, 13.4, 22.0));
+		System.out.println(m.maxOfAnything("Apple", "Grapes", "Banana"));
 	}
-
 }
