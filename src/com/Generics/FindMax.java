2742dd534dd11c6a3667de93faaf3ef75d23a58b
stutisaurya <stuti.saurya@gmail.com>
2021-06-29 13:32:20 +0530

    Added:ExtendMaximum

--- src/com/Generics/FindMax.java
+++ src/com/Generics/FindMax.java
@@ -1,6 +1,15 @@
 package com.Generics;
 
-public class FindMax {
+public class FindMax<T extends Comparable<T>> {
+
+	T x, y, z;
+
+	public FindMax(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
 	public static <T extends Comparable<T>> T checkMaximum(T x, T y, T z) {
 		T max = x;
 		if (y.compareTo(max) > 0) {
@@ -9,32 +18,33 @@ public class FindMax {
 		if (z.compareTo(max) > 0) {
 			max = z;
 		}
+		printMax(max);
 		return max;
 	}
 
-	public static <T extends Comparable<T>> T checkMaximum_With_MoreThan_ThreeParameters(T... elements) {
-		T max = elements[0];
-		for (T maximumParametereter : elements) {
-			if (maximumParametereter.compareTo(max) > 0) {
-				max = maximumParametereter;
-			}
+	public static String testMaximum(String x, String y, String z) {
+		String max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
 		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		printMax(max);
 		return max;
+	}
+
+	public static <T> void printMax(T max) {
+		System.out.println("Max of Three  is " + max);
 	}
 
 	public static void main(String[] args) {
-		Integer c = checkMaximum(10, 23, 15);
+		Integer n = checkMaximum(10, 23, 15);
 		Double d = checkMaximum(12.5, 13.4, 22.0);
-		String s = checkMaximum("Apple", "Grapes", "Banana");
-		Integer c1 = checkMaximum_With_MoreThan_ThreeParameters(10, 23, 15, 20, 24);
-		Double d1 = checkMaximum_With_MoreThan_ThreeParameters(12.5, 13.4, 22.0, 24.0, 20.8);
-		String s1 = checkMaximum_With_MoreThan_ThreeParameters("Apple", "Grapes", "Banana", "Mango", "Orange");
-		System.out.println("The maximum integer value in 10, 23, 15 is : " + c);
-		System.out.println("The maximum float value in 12.5, 13.4, 22.0 is : " + d);
-		System.out.println("The maximum float value in Apple ,Grapes, Banana is: " + s);
-		System.out.println("The maximum integer value is : " + c1);
-		System.out.println("The maximum float value is : " + d1);
-		System.out.println("The maximum string value is : " + s1);
+		String a = "Apple", b = "Grapes", c = "Banana";
+		FindMax.testMaximum(a, b, c);
+		printMax(n);
+		printMax(d);
 	}
 
 }
